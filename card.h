44e8c1d6f6d470075c28f539e6f27960ed9ac785
Zhiyi Cheng <zhiyicheng@ucsb.edu>
2026-02-14 11:18:48 -0800

    b

--- card.h
+++ card.h
@@ -1,8 +1,29 @@
 // card.h
-// Author: Your name
+// Author: Zhiyicheng
 // All class declarations related to defining a single card go here
 
 #ifndef CARD_H
 #define CARD_H
 
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Card {
+public:
+    Card(); 
+    Card(char s, string v); 
+    char getSuit() const;
+    int getVal() const;
+    bool operator<(const Card& other) const;
+    bool operator>(const Card& other) const;
+    bool operator==(const Card& other) const;
+    bool operator!=(const Card& other) const;
+    friend ostream& operator<<(ostream& a, const Card& c);
+private:
+    char suit;
+    int val;
+    int getSuitRank() const;
+};
 #endif
--- card_list.h
+++ card_list.h
@@ -1,8 +1,56 @@
 // card_list.h
-// Author: Your name
-// All class declarations related to defining a BST that represents a player's hand
+// Author: Zhiyicheng
+// // All class declarations related to defining a BST that represents a player's hand
 
 #ifndef CARD_LIST_H
 #define CARD_LIST_H
+#include "card.h"
+#include <iostream>
+
+using namespace std;
+
+class CardList {
+private:
+    struct Node {
+        Node* left;
+        Node* right;
+        Card info;
+        Node* parent; 
+
+        Node(Card c) {
+        info = c;
+       left = nullptr;right = nullptr;parent = nullptr;
+        }
+    };
+    Node* root;
+    void clear(Node* n);
+    Node* getMax(Node* n) const;
+    Node* getMin(Node* n) const;
+    Node* getPredecessor(Node* n) const;
+    Node* getSuccessor(Node* n) const;
+
+public:
+    CardList();
+    ~CardList(); 
+    void insert(Card c);
+    void remove(Card c);
+    bool contains(Card c) const;
+    class Iterator {
+    private:
+        Node* curr; 
+        friend class CardList; 
+    public:
+        Iterator(Node* n);
+        Card operator*() const;
+        Iterator& operator++();
+        Iterator& operator--();
+        bool operator==(const Iterator& other) const;
+        bool operator!=(const Iterator& other) const;
+    };
+    Iterator begin() const; 
+    Iterator end() const;  
+    Iterator rbegin() const;
+    Iterator rend() const;
+};
 
 #endif
